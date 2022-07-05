2e90bdbdf9644a0026debfe2c52ad23aa4d97a9c
Marcelo Sanches <sanches.cello@gmail.com>
2022-07-05 16:54:31 -0600

    Redeploy app with slight layout modifications & current data

--- CoronavirusShinyApp/app.R
+++ CoronavirusShinyApp/app.R
@@ -11,22 +11,41 @@ ui <- fluidPage(
     # Application title
     titlePanel("Coronavirus: Latest Country Stats"),
     
-    # Sidebar panel
+    fluidRow(
+        column(12,
+            p(
+                strong("Warning: "), 
+                "This is a personal project and not intended as a serious data analysis."
+                , style = "color:#A93C38"
+            ),
+            p(
+                tags$ul(
+                    tags$li("Explore the raw data compiled by the Johns Hopkins University Center for 
+                            Systems Science and Engineering in this ",
+                            tags$a(href="https://github.com/CSSEGISandData/COVID-19", "JHU CSSE GitHub repository.")),
+                    tags$li("See ",
+                            tags$a(href="https://github.com/BigBangData/CoronavirusDataAnalysis", "my GitHub repository"),
+                            " for all files and code related to this app.")
+                )
+            )
+        )
+    ),
+
     sidebarLayout(
-        position = "right",
+        position = "left",
         sidebarPanel(
             
             # drop-down for type of plot
             selectInput(inputId = "plot_type", 
                         label = "Plot Type",
                         choices = c( "Total Count", "Count per 10K", "New Cases per 10K"),
-                        selected = "Count Per 10K"),
+                        selected = "Total Count"),
             
             # drop-down for status
             selectInput(inputId = "status", 
                         label = "Status",
                         choices = c("Confirmed", "Fatal"),
-                        selected = "Fatal"),
+                        selected = "Confirmed"),
             
             # slider for top N number of countries to display
             sliderInput(inputId = "top_n",
@@ -38,38 +57,16 @@ ui <- fluidPage(
         
         # Show plot
         mainPanel(
-           plotOutput("distPlot")
-        )
-    ),
-    
-    fluidRow(
-        column(12,
-            p(
-                strong("Warning! "),
-                "These bar charts are not intended as a serious data analysis, which would require more data and study.
-                This is a personal project to explore the daily JHU datasets.", style = "color:#A93C38"
-            ),
-            h4(strong("Notes")),
-            p(
-                tags$ul(
-                    tags$li("This is a simple exploration of the raw time series data compiled by the Johns Hopkins University Center for 
-                            Systems Science and Engineering (JHU CCSE) from various sources."),
-                    tags$li("See the ",
-                            tags$a(href="https://github.com/CSSEGISandData/COVID-19", "JHU-CSSE GitHub repository"),
-                            " for more information."),
-                    tags$li("See ",
-                            tags$a(href="https://github.com/BigBangData/CoronavirusDataAnalysis", "this GitHub repository"),
-                            " for all files and code related to this app and data project.")
-                )
-            )
+           plotOutput("barplots")
         )
     )
+
 )
 
 # Define server logic
 server <- function(input, output) {
     
-    output$distPlot <- renderPlot({
+    output$barplots <- renderPlot({
         
         # subset to specific type
         data <- current_data[current_data$Type == input$plot_type, ]
@@ -92,6 +89,7 @@ server <- function(input, output) {
             theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 10)
         )
     })
+
 }
 
 # Run the application 
